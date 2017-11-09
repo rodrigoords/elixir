2a62252adef62e472f001c50a98e0a2c7c1e422c
Rodrigo Sene <rodrigo.sene@qisi.com.br>
2017-11-09 00:04:07 -0200

    create deck List.flatten(List) approach

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -15,11 +15,12 @@ defmodule Cards do
   def create_deck do
     values = ["Jack", "Queen", "King", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-    for value <- values do      
-      for suit <- suits do
-        "#{value} of #{suit}"
-      end
-    end
+    deck = for value <- values do      
+              for suit <- suits do
+                "#{value} of #{suit}"
+              end
+            end
+    List.flatten(deck);
   end
 
   def shuffer(deck) do
