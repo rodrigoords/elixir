6d04aed3eb70b197bf476d42abd99f098e27028a
Rodrigo Sene <rodrigo.sene@qisi.com.br>
2017-11-10 19:53:30 -0200

    implement save lead and drop_hand methods

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -31,4 +31,23 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do 
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "Error to read file, Maybe that file does not exist."
+    end
+  end
+
+  def drop_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffer
+    |> Cards.deal(hand_size)
+  end
+
 end
