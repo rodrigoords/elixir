a2ccfe8ccc11b9ee7d3e1787b8d84d28531ca414
Rodrigo Sene <rodrigo.sene@qisi.com.br>
2017-11-17 21:06:40 -0200

    create tests to Cards modules

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,4 +1,21 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create deck must return 52 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck rendomizes it " do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffler(deck)
+  end
+
+  test "drop hand must drop the number of cards pass and the remeing cards" do
+    { hand, deck } = Cards.drop_hand 2
+    assert length(hand) == 2
+    assert length(deck) == 50
+  end
+
 end
