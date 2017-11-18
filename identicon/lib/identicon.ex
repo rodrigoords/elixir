e19c1b9897e7eb6fcf1931f8d3717bc6aec30c47
Rodrigo Sene <rodrigo.sene@qisi.com.br>
2017-11-17 22:02:05 -0200

    create identicon project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,13 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+  end
+end
